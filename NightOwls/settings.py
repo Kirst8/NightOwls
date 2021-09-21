690fcffd0ea0f4c4cb4f92abc60c2c1ac6ff9a4c
Kirst8 <kirst8.brooker@gmail.com>
2021-09-21 12:13:56 +0000

    setting.py change in debug and secret key

--- NightOwls/settings.py
+++ NightOwls/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&txjg535td&iib@%=ka42&32p5hbe)j0)d92m4_iclozb=0(-u'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kirst8-night-owls.herokuapp.com','localhosts']
 
