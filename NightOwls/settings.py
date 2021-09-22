244056ec536eca494b6caa4632efd465d5c1ddfc
Kirst8 <kirst8.brooker@gmail.com>
2021-09-22 08:03:40 +0000

    deployment fix

--- NightOwls/settings.py
+++ NightOwls/settings.py
@@ -198,6 +198,8 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
     
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'euro'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
@@ -206,8 +208,3 @@ DEFAULT_FROM_EMAIL = 'Kirst95.brooker@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DISCOUNT_THRESHOLD = 50
-DISCOUNT_PERCENTAGE = 10
