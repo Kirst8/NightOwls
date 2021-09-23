17ed9faaffdc16cde7e4d534d6613a9a1cb5f74c
Kirst8 <kirst8.brooker@gmail.com>
2021-09-23 08:28:05 +0000

    main nav fix centered

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import (
+    render, reverse, redirect, get_object_or_404
+)
+
 from .models import BlogPost, Comment
 from .forms import CommentForm
 from django.contrib import messages
